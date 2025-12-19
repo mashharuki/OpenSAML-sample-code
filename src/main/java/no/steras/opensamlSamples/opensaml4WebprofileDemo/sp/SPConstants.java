ace9e20f3aad55b259eafe65cae443c9a4cb117c
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-12-19 23:33:30 +0900

    fix

--- src/main/java/no/steras/opensamlSamples/opensaml4WebprofileDemo/sp/SPConstants.java
+++ src/main/java/no/steras/opensamlSamples/opensaml4WebprofileDemo/sp/SPConstants.java
@@ -8,6 +8,6 @@ public class SPConstants {
 	public static final String AUTHENTICATED_SESSION_ATTRIBUTE = "authenticated";
 	public static final String GOTO_URL_SESSION_ATTRIBUTE = "gotoURL";
 	// ACS URL
-	public static final String ASSERTION_CONSUMER_SERVICE = "http://localhost:8080/opensaml4-webprofile-demo/sp/consumer";
+	public static final String ASSERTION_CONSUMER_SERVICE = "http://localhost:8080/opensaml5-webprofile-demo/sp/consumer";
 
 }
