d731e0c3a24eaf9d81df2a9d3152b16e670b89a0
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-12-20 10:28:30 +0900

    コメント追加

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,12 +1,21 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
 import * as apigatewayv2_integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
 import * as path from 'path';
 
+/**
+ * APIGateway + Lambda(LambdaWebAdapterを採用)
+ */
 export class CdkStack extends cdk.Stack {
+  /**
+   * コンストラクター
+   * @param scope 
+   * @param id 
+   * @param props 
+   */
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -15,19 +24,22 @@ export class CdkStack extends cdk.Stack {
     const baseUrl = this.node.tryGetContext('baseUrl') || '';
 
     // Build Docker image for Lambda
+    // LambdaWebAdapter上で動かすWebアプリ用のDockerコンテナイメージ
     const dockerImage = new ecr_assets.DockerImageAsset(this, 'SamlSpringBootImage', {
       directory: path.join(__dirname, '../../backend'),
       platform: ecr_assets.Platform.LINUX_AMD64,
     });
 
-    // Lambda function using Lambda Web Adapter
+    // LambdaWebAdapter上で動かすWebアプリ用のLambda関数
     const samlFunction = new lambda.DockerImageFunction(this, 'SamlSpringBootFunction', {
+      // Dockerコンテナイメージ
       code: lambda.DockerImageCode.fromEcr(dockerImage.repository, {
         tagOrDigest: dockerImage.imageTag,
       }),
       memorySize: 1024,
       timeout: cdk.Duration.seconds(30),
       architecture: lambda.Architecture.X86_64,
+      // 環境変数
       environment: {
         // Lambda Web Adapter settings
         PORT: '8080',
@@ -41,7 +53,7 @@ export class CdkStack extends cdk.Stack {
       description: 'SAML Spring Boot application with Lambda Web Adapter',
     });
 
-    // HTTP API Gateway
+    // HTTP API Gatewayリソース
     const httpApi = new apigatewayv2.HttpApi(this, 'SamlHttpApi', {
       apiName: 'saml-spring-boot-api',
       description: 'HTTP API for SAML Spring Boot application',
@@ -52,25 +64,29 @@ export class CdkStack extends cdk.Stack {
       },
     });
 
-    // Lambda integration
+    // LambdaWebAdapter上で動かすWebアプリ用のLambda関数とHTTP API Gatewayの統合
     const lambdaIntegration = new apigatewayv2_integrations.HttpLambdaIntegration(
       'SamlLambdaIntegration',
       samlFunction
     );
 
-    // Add routes - proxy all requests to Lambda
+    // Add routes - HTTP API Gatewayリソースに対して、LambdaWebAdapter上で動かすWebアプリ用のLambda関数と統合
     httpApi.addRoutes({
       path: '/{proxy+}',
       methods: [apigatewayv2.HttpMethod.ANY],
       integration: lambdaIntegration,
     });
 
-    // Root path route
+    // APIGateway ルートパス
     httpApi.addRoutes({
       path: '/',
       methods: [apigatewayv2.HttpMethod.ANY],
       integration: lambdaIntegration,
     });
+
+    // ※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※
+    // CDKスタックの成果物
+    // ※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※
 
     // Outputs
     new cdk.CfnOutput(this, 'ApiEndpoint', {
